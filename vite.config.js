71bab7d22d660685cada85ea414d4e5f64ea625d
ibrahim najjar <najjaribrahim707@gmail.com>
2025-11-20 12:21:02 +0200

    change

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: import.meta.env.VITE_BASE_PATH || "/zafer-final-project"
+  // eslint-disable-next-line no-undef
+  base:process.env.VITE_BASE_PATH || "/zafer-final-project"
+
 })
